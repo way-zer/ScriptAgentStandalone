e45769d971ce255ed91a6c7e43382277783e0554
way-zer <himc.wicp@gmail.com>
2020-12-21 10:30:49 +0800

    [Update](loader) 添加nativeLibs支持

--- plugin/src/cf/wayzer/scriptAgent/Main.kt
+++ plugin/src/cf/wayzer/scriptAgent/Main.kt
@@ -2,10 +2,15 @@ package cf.wayzer.scriptAgent
 
 import cf.wayzer.script_agent.ScriptManager
 import java.io.File
+import java.net.URL
+import java.net.URLClassLoader
 
 object Main {
     @JvmStatic
     fun main() {
+        URLClassLoader::class.java.getDeclaredMethod("addURL", URL::class.java).apply {
+            isAccessible = true
+        }.invoke(javaClass.classLoader, File("nativeLibs").toURI().toURL())
         ScriptManager.loadDir(File("scripts").apply { mkdir() })
         println("Finished!")
         while (true) Thread.sleep(60_000)
