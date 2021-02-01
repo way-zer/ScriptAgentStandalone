ea063e9dfe371f062dd326209cfab7719ba967fb
way-zer <himc.wicp@gmail.com>
2021-02-01 22:04:58 +0800

    [Update](loader) 升级库到1.4.2 [Update](coreLib) 升级exposed到0.29.1

--- dependencies.gradle.kts
+++ dependencies.gradle.kts
@@ -1,4 +1,4 @@
-val libraryVersion = "1.4.1"
+val libraryVersion = "1.4.2"
 
 repositories {
 //    maven("http://maven.aliyun.com/nexus/content/groups/public/")
@@ -21,9 +21,9 @@ dependencies {
     //coreLibrary
     compile("cf.wayzer:PlaceHoldLib:2.1.0")
     compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.1")
-    compile("org.jetbrains.exposed:exposed-core:0.24.1")
-    compile("org.jetbrains.exposed:exposed-dao:0.24.1")
-    compile("org.jetbrains.exposed:exposed-java-time:0.24.1")
+    compile("org.jetbrains.exposed:exposed-core:0.29.1")
+    compile("org.jetbrains.exposed:exposed-dao:0.29.1")
+    compile("org.jetbrains.exposed:exposed-java-time:0.29.1")
     compile("io.github.config4k:config4k:0.4.1")
     //coreStandalone
     compile("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.2.1")
