ee8550beda89ef20bca796ca1a504c23911c4785
way-zer <himc.wicp@gmail.com>
2021-04-14 19:07:40 +0800

    :arrow_up: (loader) 升级库到v1.7.1 -- 优化性能,仅在编译时加载编译器，生成cacheJar，进一步提高ktc加载效率 -- 优化细节，减少loader初始化的频率 -- 关于其他v1.7的更新,可以见SA4Mindustry更新日记

--- plugin/src/cf/wayzer/scriptAgent/loader.kt
+++ plugin/src/cf/wayzer/scriptAgent/loader.kt
@@ -3,10 +3,12 @@ package cf.wayzer.scriptAgent
 import cf.wayzer.scriptAgent.define.LoaderApi
 
 @OptIn(LoaderApi::class)
-fun main() {
+fun main(args: Array<String>?) {
     if (System.getProperty("java.util.logging.SimpleFormatter.format") == null)
         System.setProperty("java.util.logging.SimpleFormatter.format", "[%1\$tF | %1\$tT | %4\$s] [%3\$s] %5\$s%6\$s%n")
     ScriptAgent.loadUseClassLoader()?.apply {
-        loadClass("cf.wayzer.scriptAgent.Main").getMethod("main").invoke(null)
+        loadClass("cf.wayzer.scriptAgent.Main")
+            .getMethod("main", Array<String>::class.java)
+            .invoke(null, args)
     }
 }
