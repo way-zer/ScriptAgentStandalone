73f6088a5256659eba0e4e7c072c60c8459a7086
way-zer <himc.wicp@gmail.com>
2021-04-14 19:07:40 +0800

    :arrow_up: (loader) 升级库到v1.7.1 -- 优化性能,仅在编译时加载编译器，生成cacheJar，进一步提高ktc加载效率 -- 优化细节，减少loader初始化的频率 -- 关于其他v1.7的更新,可以见SA4Mindustry更新日记

--- plugin/src/cf/wayzer/scriptAgent/Main.kt
+++ plugin/src/cf/wayzer/scriptAgent/Main.kt
@@ -1,19 +1,45 @@
 package cf.wayzer.scriptAgent
 
 import cf.wayzer.scriptAgent.define.LoaderApi
+import cf.wayzer.scriptAgent.define.ScriptState
 import java.io.File
 import java.net.URL
 import java.net.URLClassLoader
+import kotlin.system.exitProcess
 
+@OptIn(LoaderApi::class)
 object Main {
     @JvmStatic
-    @OptIn(LoaderApi::class)
-    fun main() {
+    fun main(args: Array<String>) {
         URLClassLoader::class.java.getDeclaredMethod("addURL", URL::class.java).apply {
             isAccessible = true
         }.invoke(javaClass.classLoader, File("nativeLibs").toURI().toURL())
+        if (args.isNotEmpty()) generateMain(args)
         ScriptManager.loadDir(File("scripts").apply { mkdir() })
         println("Finished!")
         while (true) Thread.sleep(60_000)
+    }
+
+    @JvmStatic
+    private fun generateMain(args: Array<String>) {
+        Config.rootDir = File("scripts")
+        var notFound = 0
+        if (args.isEmpty())
+            ScriptManager.loadDir(Config.rootDir, enable = false)
+        else
+            args.forEach {
+                val script = ScriptManager.getScriptNullable(it)
+                if (script == null) {
+                    println("找不到脚本: $it")
+                    notFound++
+                    return@forEach
+                }
+                ScriptManager.loadScript(script, enable = false, children = false)
+            }
+        val fail = ScriptManager.allScripts.count { it.value.scriptState == ScriptState.Fail }
+        if (notFound != 0)
+            println("有${notFound}个输入脚本未找到")
+        println("共加载${ScriptManager.allScripts.size}个脚本，失败${fail}个")
+        exitProcess(notFound + fail)
     }
 }
