27a457262a113a9505fcf734a3186cf05f45f967
way-zer <himc.wicp@gmail.com>
2021-09-09 15:58:55 +0800

    :sparkles: (mindustryProxy/trafficAnalysis) 增加数据包数量统计

--- scripts/mindustryProxy/trafficAnalysis.kts
+++ scripts/mindustryProxy/trafficAnalysis.kts
@@ -4,29 +4,55 @@ import mindustryProxy.lib.event.PlayerPacketEvent
 import mindustryProxy.lib.packet.StreamChunk
 import mindustryProxy.lib.packet.UnknownPacket
 
-var nameMap = emptyArray<String>()
-var traffic = emptyArray<Int>()
+data class Data(val id: Int, val name: String, var traffic: Int = 0, var packetNum: Int = 0) : Comparable<Data> {
+    override fun compareTo(other: Data): Int {
+        return compareValuesBy(this, other,
+            { it.traffic },
+            { it.packetNum }
+        )
+    }
+
+    fun reset() {
+        traffic = 0
+        packetNum = 0
+    }
+
+    override fun toString(): String {
+        return "${id.toString().padStart(2)}  ${name.padEnd(30)}: ${traffic / 1000}KB(P$packetNum)"
+    }
+}
+
+var data = emptyArray<Data>()
 var beginTime = System.currentTimeMillis()
 
 onEnable {
-    val list = sourceFile.resolveSibling("res/remoteId.csv").useLines { it.toList() }
-    nameMap = Array(list.size) { "unknown" }
-    traffic = Array(list.size) { 0 }
-    list.forEach {
-        val sp = it.split("\t")
-        nameMap[sp[0].toInt()] = sp[1]
+    val list = sourceFile.resolveSibling("res/remoteId.csv").useLines { lines ->
+        lines.associate {
+            val sp = it.split("\t")
+            sp[0].toInt() to sp[1]
+        }
+    }
+    if (list.isEmpty()) return@onEnable
+    data = Array(list.keys.maxOrNull()!! + 1) {
+        Data(it, list[it] ?: "Unknown")
     }
 }
 
 listenTo<PlayerPacketEvent> {
-    if (nameMap.isEmpty()) return@listenTo
+    if (data.isEmpty()) return@listenTo
     when (val packet = packet) {
         is UnknownPacket -> {
-            if (packet.typeId >= nameMap.size) return@listenTo
-            traffic[packet.typeId] += packet.data.readableBytes()
+            if (packet.typeId >= data.size) return@listenTo
+            data[packet.typeId].apply {
+                traffic += packet.data.readableBytes()
+                packetNum++
+            }
         }
         is StreamChunk -> {
-            traffic[packet.factory.packetId!!] += packet.data.readableBytes()
+            data[packet.factory.packetId!!].apply {
+                traffic += packet.data.readableBytes()
+                packetNum++
+            }
         }
     }
 }
@@ -34,18 +60,16 @@ listenTo<PlayerPacketEvent> {
 command("traffic", "查看InvokePacket流量") {
     usage = "[reset]"
     body {
-        if (nameMap.isEmpty())
+        if (data.isEmpty())
             returnReply("[red]资源csv文件未加载".with())
         if (arg.isNotEmpty() && arg[0] == "reset") {
-            traffic.fill(0)
+            data.forEach(Data::reset)
             beginTime = System.currentTimeMillis()
             reply("[green]Reset finish".with())
         } else {
-            val list = traffic.mapIndexed { index, i ->
-                (index.toString().padStart(2) + " " + nameMap[index]) to i
-            }.sortedByDescending { it.second }
-                .map { (name, i) -> "$name:${i / 1024}KB" }
+            val list = data.sortedDescending()
                 .subList(0, 20)
+                .map { it.toString() }
             reply(
                 "InvokePacket Traffic in last {time}s:\n{list:\n}".with(
                     "time" to (System.currentTimeMillis() - beginTime) / 1000,
