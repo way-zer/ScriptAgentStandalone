4d3c8b0ac52e3d7ac1618a766f9700b86db49320
way-zer <himc.wicp@gmail.com>
2021-09-18 19:07:28 +0800

    :arrow_up: (loader) 更新库到v1.7.5 :arrow_up: (coreLib) 从SA4Mindustry导入更新 :sparkles: (coreLib/ServiceRegistry) 使用flow代替channel

--- scripts/coreLibrary/DBApi.kts
+++ scripts/coreLibrary/DBApi.kts
@@ -1,11 +1,11 @@
-@file:Import("org.jetbrains.exposed:exposed-core:0.29.1", mavenDepends = true)
-@file:Import("org.jetbrains.exposed:exposed-dao:0.29.1", mavenDepends = true)
-@file:Import("org.jetbrains.exposed:exposed-java-time:0.29.1", mavenDepends = true)
-@file:Import("org.jetbrains.exposed:exposed-jdbc:0.29.1", mavenDepends = true)
+@file:Import("org.jetbrains.exposed:exposed-core:0.30.1", mavenDepends = true)
+@file:Import("org.jetbrains.exposed:exposed-dao:0.30.1", mavenDepends = true)
+@file:Import("org.jetbrains.exposed:exposed-java-time:0.30.1", mavenDepends = true)
+@file:Import("org.jetbrains.exposed:exposed-jdbc:0.30.1", mavenDepends = true)
 
 package coreLibrary
 
-import coreLibrary.lib.util.Provider
+import coreLibrary.lib.util.ServiceRegistry
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
@@ -13,18 +13,18 @@ import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 
 
-object DB {
+@Suppress("unused")
+object DB : ServiceRegistry<Database>() {
     private val ISubScript.registeredTable by dataKeyWithDefault { mutableSetOf<Table>() }
-    val db = Provider<Database>()
 
     /**
      * 为模块注册表格
      * 注册时不一定立刻运行
-     * 会等[db]初始化后统一注册
+     * 会等[DB]初始化后统一注册
      */
     fun ISubScript.registerTable(vararg t: Table) {
         registeredTable.addAll(t)
-        db.listenWithAutoCancel(this) {
+        subscribe(this) {
             transaction(it) {
                 SchemaUtils.createMissingTablesAndColumns(*t)
             }
@@ -32,6 +32,6 @@ object DB {
     }
 }
 
-DB.db.every {
+DB.subscribe(this) {
     TransactionManager.defaultDatabase = it
 }
